72226191b098a06f7aab1f84b219abaa097b6569
1939108122 <1939108122@qq.com>
2019-12-07 21:34:20 +0800

    login

---
+++ WeChat/du_program/miniprogram/pages/buy/buy.js
@@ -0,0 +1,17 @@
+Page({
+  data:{
+    "slides" : [
+     { 
+       "image":"cloud://hawkkke.6861-hawkkke-1300705540/1A2CD3D1B9BB753D1C01675BAC8C2F62.jpg"
+     },
+     { 
+      "image": "cloud://hawkkke.6861-hawkkke-1300705540/8642C51E787217679259232BF509CD12.jpg"
+    },
+    { 
+      "image":"cloud://hawkkke.6861-hawkkke-1300705540/201129532B92A2BDC9ED571FB9731C82.jpg"
+    }
+
+    ],
+    
+  }
+})

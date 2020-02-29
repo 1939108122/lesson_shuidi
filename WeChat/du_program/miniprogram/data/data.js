bbe190cc7600fdfee7afb1d705d32ebc40cebf48
1939108122 <1939108122@qq.com>
2020-02-29 20:43:57 +0800

    du

---
+++ WeChat/du_program/miniprogram/data/data.js
@@ -0,0 +1,50 @@
+export default{
+    slides:[
+        { 
+          image:"cloud://hawkkke.6861-hawkkke-1300705540/1A2CD3D1B9BB753D1C01675BAC8C2F62.jpg"
+        },
+        { 
+         image: "cloud://hawkkke.6861-hawkkke-1300705540/8642C51E787217679259232BF509CD12.jpg"
+       },
+       {
+         image:"cloud://hawkkke.6861-hawkkke-1300705540/201129532B92A2BDC9ED571FB9731C82.jpg"
+       }
+   
+       ],
+       Newthings: [
+        {image:"cloud://hawkkke.6861-hawkkke-1300705540/新品/新品1.jpg",describe:"Nike Air Force 1 QS City of Dreams 绿",
+        price:"￥939"},
+        {image:"cloud://hawkkke.6861-hawkkke-1300705540/新品/新品2.jpg",describe:"Nike Dunk Low 'Plum' 树莓紫 梅子",
+        price:"￥1539"},
+        {image:"cloud://hawkkke.6861-hawkkke-1300705540/新品/新品3.jpg",describe:"Nike SB Dunk Low Pro Iso Infrared 灰橙",
+        price:"￥1599"},
+        {image:"cloud://hawkkke.6861-hawkkke-1300705540/新品/新品4.jpg",describe:"Strangelove x Nike SB Dunk Low Pro QS 情人",
+        price:"￥4169"},
+        {image:"cloud://hawkkke.6861-hawkkke-1300705540/新品/新品9.jpg",describe:"【2月15日21点原价限时抢购】 xVESSEL G.O.P",
+        price:"￥--"},
+        {image:"cloud://hawkkke.6861-hawkkke-1300705540/新品/新品10.jpg",describe:"Vans Sk8-Mid I Heart 棋盘格 情人节限定",
+        price:"￥539"},
+        {image:"cloud://hawkkke.6861-hawkkke-1300705540/新品/新品11.jpg",describe:"ESTEE LAUDER 雅诗兰黛 情人节限定唇膏礼盒",
+        price:"￥849"},
+        {image:"cloud://hawkkke.6861-hawkkke-1300705540/新品/新品12.jpg",describe:"YSL 圣罗兰 红唇呼叫限量方管唇膏 N° 114 枣红",
+        price:"￥399"}
+      ],
+      Newthings2: [
+        {image:"cloud://hawkkke.6861-hawkkke-1300705540/新品/新品5.jpg",describe:"Air Jordan 1 High '85 'Varsity Red' 反转黑红",
+        price:"￥9489"},
+        {image:"cloud://hawkkke.6861-hawkkke-1300705540/新品/新品6.jpg",describe:"adidas originals Big trefoil Track Top",
+        price:"￥589"},
+        {image:"cloud://hawkkke.6861-hawkkke-1300705540/新品/新品7.jpg",describe:"Nike Kyrie 6 EP 'Oracle Aqua' 清爽蓝",
+        price:"￥949"},
+        {image:"cloud://hawkkke.6861-hawkkke-1300705540/新品/新品8.jpg",describe:"adidas originals Superstar 白黑金 女款",
+        price:"￥539"},
+        {image:"cloud://hawkkke.6861-hawkkke-1300705540/新品/新品13.jpg",describe:"STONE ISLAND Logo刺绣套头卫衣 黑色",
+        price:"￥2199"},
+        {image:"cloud://hawkkke.6861-hawkkke-1300705540/新品/新品14.jpg",describe:"中国李宁 巴黎时装走秀款 宽松套头连帽卫衣 黑",
+        price:"￥499"},
+        {image:"cloud://hawkkke.6861-hawkkke-1300705540/新品/新品15.jpg",describe:"adidas originals 情人节系列 Unisex Hoodie",
+        price:"￥549"},
+        {image:"cloud://hawkkke.6861-hawkkke-1300705540/新品/新品16.jpg",describe:"【吴亦凡同款】LOUISVUITTON SS20",
+        price:"￥8589"}
+      ],
+}
--- WeChat/du_program/miniprogram/pages/buy/buy.js
+++ WeChat/du_program/miniprogram/pages/buy/buy.js
@@ -1,17 +1,20 @@
+const app = getApp();
 Page({
   data:{
-    "slides" : [
-     { 
-       "image":"cloud://hawkkke.6861-hawkkke-1300705540/1A2CD3D1B9BB753D1C01675BAC8C2F62.jpg"
-     },
-     { 
-      "image": "cloud://hawkkke.6861-hawkkke-1300705540/8642C51E787217679259232BF509CD12.jpg"
-    },
-    { 
-      "image":"cloud://hawkkke.6861-hawkkke-1300705540/201129532B92A2BDC9ED571FB9731C82.jpg"
-    }
-
-    ],
-    
-  }
+    active:0, 
+    slides:[],
+    Newthings:[],
+    Newthings2:[]
+  
+  },
+  onLoad() {
+    const slides = app.globalData.slides,
+       Newthings = app.globalData.Newthings,
+       Newthings2= app.globalData.Newthings2;
+    this.setData({
+      slides,
+      Newthings,
+      Newthings2,
+    })
+  },
 })

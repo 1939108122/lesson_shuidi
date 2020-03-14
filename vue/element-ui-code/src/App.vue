53967df4750a3fc74abb5cb8ac5ef78a9406a84e
1939108122 <1939108122@qq.com>
2020-03-14 22:14:37 +0800

    Element-UI源码Button分析

---
+++ vue/element-ui-code/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <el-button round  plain type="success" icon="el-icon-check"> <span slot="ddd"> 按钮 </span></el-button>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+  import ElButton from './components/ElButton.vue'
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    ElButton
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/element-ui-code/src/components/ElButton.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <button class="el-button" 
+      :class="[type ? 'el-button--' + type:'', 
+      {
+          'is-round': round,
+          'is-plain': plain
+      }
+      ]" >
+        <!-- ElButton -->
+        <i :class="icon" v-if="icon" ></i>
+        <span v-if="$slots.default"><slot name="ddd"></slot></span>
+      </button>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        type:{
+            type: String,
+            default: 'default'
+        },
+        round:{
+            type: Boolean,
+            default: false
+        },
+        plain:{
+            type: Boolean,
+            default: false
+        },
+        icon: String
+    }
+    
+}
+</script>
+<style>
+
+</style>

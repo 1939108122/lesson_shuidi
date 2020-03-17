5cec59b671645112dbef40ec59d2cc7a2fa36385
1939108122 <1939108122@qq.com>
2020-03-17 23:29:02 +0800

    力扣twoSum

---
+++ leet_code/countCharacters/2.js
@@ -0,0 +1,24 @@
+var countCharacters = function(words, chars) {
+    var cnt = 0;
+    words.forEach(word => {
+      var char = chars;
+      var len = 0;
+      for(var i = 0;i<word.length;i++){
+        if(char.indexOf(word[i])!=-1){
+          len +=1;
+          char = char.replace(new RegExp(word[i]),'+')  //将char表中有word[i]的字母换成其他，例如加号，防止出现下一次有又匹配到这个字母
+        }else{
+          break
+        }
+      }
+      if(len==word.length){
+        cnt+=word.length
+      }
+    });
+    return cnt
+  };
+  var words = ["cat","bt","hat","tree"]
+  var chars = "atach"
+console.log(countCharacters(words, chars))
+// console.log(countCharacters(["dyiclysmffuhibgfvapygkorkqllqlvokosagyelotobicwcmebnpznjbirzrzsrtzjxhsfpiwyfhzyonmuabtlwin","ndqeyhhcquplmznwslewjzuyfgklssvkqxmqjpwhrshycmvrb","ulrrbpspyudncdlbkxkrqpivfftrggemkpyjl","boygirdlggnh","xmqohbyqwagkjzpyawsydmdaattthmuvjbzwpyopyafphx","nulvimegcsiwvhwuiyednoxpugfeimnnyeoczuzxgxbqjvegcxeqnjbwnbvowastqhojepisusvsidhqmszbrnynkyop","hiefuovybkpgzygprmndrkyspoiyapdwkxebgsmodhzpx","juldqdzeskpffaoqcyyxiqqowsalqumddcufhouhrskozhlmobiwzxnhdkidr","lnnvsdcrvzfmrvurucrzlfyigcycffpiuoo","oxgaskztzroxuntiwlfyufddl","tfspedteabxatkaypitjfkhkkigdwdkctqbczcugripkgcyfezpuklfqfcsccboarbfbjfrkxp","qnagrpfzlyrouolqquytwnwnsqnmuzphne","eeilfdaookieawrrbvtnqfzcricvhpiv","sisvsjzyrbdsjcwwygdnxcjhzhsxhpceqz","yhouqhjevqxtecomahbwoptzlkyvjexhzcbccusbjjdgcfzlkoqwiwue","hwxxighzvceaplsycajkhynkhzkwkouszwaiuzqcleyflqrxgjsvlegvupzqijbornbfwpefhxekgpuvgiyeudhncv","cpwcjwgbcquirnsazumgjjcltitmeyfaudbnbqhflvecjsupjmgwfbjo","teyygdmmyadppuopvqdodaczob","qaeowuwqsqffvibrtxnjnzvzuuonrkwpysyxvkijemmpdmtnqxwekbpfzs","qqxpxpmemkldghbmbyxpkwgkaykaerhmwwjonrhcsubchs"]
+// ,"usdruypficfbpfbivlrhutcgvyjenlxzeovdyjtgvvfdjzcmikjraspdfp"))

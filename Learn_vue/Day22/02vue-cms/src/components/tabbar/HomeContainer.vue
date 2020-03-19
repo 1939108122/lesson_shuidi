4621058c904eae154227865b5a5fda430301377f
1939108122 <1939108122@qq.com>
2020-03-19 18:01:14 +0800

    请求数据

--- Learn_vue/Day22/02vue-cms/src/components/tabbar/HomeContainer.vue
+++ Learn_vue/Day22/02vue-cms/src/components/tabbar/HomeContainer.vue
@@ -1,13 +1,76 @@
 <template>
     <div>
-        homecontainer
+      <mt-swipe :auto="4000">
+        <mt-swipe-item v-for="item in lunbotuList" :key="item.img">
+           <img :src="item.img" alt="">
+        </mt-swipe-item>
+      </mt-swipe>
+        <ul class="mui-table-view mui-grid-view mui-grid-9">
+          <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+            <span class="mui-icon mui-icon-home"></span>
+            <div class="mui-media-body">Home</div></a></li>
+          <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+            <span class="mui-icon mui-icon-email"><span class="mui-badge">5</span></span>
+            <div class="mui-media-body">Email</div></a></li>
+          <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+            <span class="mui-icon mui-icon-chatbubble"></span>
+            <div class="mui-media-body">Chat</div></a></li>
+          <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+            <span class="mui-icon mui-icon-location"></span>
+            <div class="mui-media-body">location</div></a></li>
+          <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+            <span class="mui-icon mui-icon-search"></span>
+            <div class="mui-media-body">Search</div></a></li>
+          <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+            <span class="mui-icon mui-icon-phone"></span>
+            <div class="mui-media-body">Phone</div></a></li>
+        </ul> 
     </div>
 </template>
 <script>
+import { Toast } from 'mint-ui'
 export default {
-    
+    data() {
+      return {
+        lunbotuList: []
+      }
+    },
+    created() {
+      this.getLunbotu()
+    },
+    methods: {
+      getLunbotu() {
+        this.$http.get('http://www.liulongbin.top:3005/api/getlunbo').then(result=> {
+          // console.log(result.body);
+          if(result.body.status === 0)
+          {
+            this.lunbotuList = result.body.message
+          }
+          else{
+            Toast('加载轮播图失败')
+          }
+        })
+      }
+    }
 }
 </script>
-<style>
-
+<style lang="scss">
+.mint-swipe{
+    height: 200px;
+    .mint-swipe-item{
+      &:nth-child(1){
+          background-color: red;
+      }
+       &:nth-child(2){
+          background-color: blue;
+      }
+       &:nth-child(3){
+          background-color: cyan;
+      }
+      img{
+        width: 100%;
+        height: 100%;
+      }
+    }
+}
 </style>
--- Learn_vue/Day22/02vue-cms/src/main.js
+++ Learn_vue/Day22/02vue-cms/src/main.js
@@ -1,11 +1,15 @@
 //入口文件 
 import Vue from 'vue'
 import app from './App.vue'
-import { Header } from 'mint-ui';
+import { Header,Swipe, SwipeItem } from 'mint-ui';
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 import './lib/mui/css/mui.min.css'
 import './lib/mui/css/icons-extra.css'
 import VueRouter from 'vue-router'
 import router from './router.js'
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
 Vue.use(VueRouter)
 Vue.component(Header.name, Header);
 var vm = new Vue({

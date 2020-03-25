7aa2912e7ef8a5d516a744d306e3290009862bd3
1939108122 <1939108122@qq.com>
2020-03-25 11:20:55 +0800

    新闻详情路由

---
+++ Learn_vue/Day22/02vue-cms/src/components/news/NewsInfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>新闻详情---- {{ id }}</h3>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      id : this.$route.params.id
+    }
+  }
+}
+</script>
+<style>
+
+</style>
--- Learn_vue/Day22/02vue-cms/src/router.js
+++ Learn_vue/Day22/02vue-cms/src/router.js
@@ -4,14 +4,16 @@ import MemberContainer from './components/tabbar/MemberContainer.vue'
 import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'
 import NewsList from './components/news/NewsList.vue'
+import NewsInfo from './components/news/NewsInfo.vue'
 var router = new VueRouter({
     routes: [
         {path: '/', redirect:'/home'},
-        {path: '/home', component: HomeContainer,},
+        {path: '/home', component: HomeContainer},
         {path: '/member', component: MemberContainer},
         {path: '/shopcar', component: ShopcarContainer},
         {path: '/search', component: SearchContainer},
-        {path: '/home/newslist', component: NewsList }
+        {path: '/home/newslist', component: NewsList },
+        {path: '/home/newsinfo/:id', component: NewsInfo }
 
     ],
     linkActiveClass: 'mui-active' //覆盖默认的路由高亮的类  默认的类为link-active-class

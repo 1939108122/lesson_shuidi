995cadcef9e3e56fa9725f9f1c292166bcca2349
1939108122 <1939108122@qq.com>
2020-03-28 18:11:58 +0800

    图片导航栏制作

--- Learn_vue/Day22/02vue-cms/src/components/tabbar/HomeContainer.vue
+++ Learn_vue/Day22/02vue-cms/src/components/tabbar/HomeContainer.vue
@@ -8,22 +8,22 @@
         <ul class="mui-table-view mui-grid-view mui-grid-9">
           <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="/home/newslist">
             <span class="mui-icon mui-icon-home"></span>
-            <div class="mui-media-body">Home</div></router-link></li>
-          <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+            <div class="mui-media-body">新闻资讯</div></router-link></li>
+          <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to= "/home/photolist">
             <span class="mui-icon mui-icon-email"><span class="mui-badge">5</span></span>
-            <div class="mui-media-body">Email</div></a></li>
+            <div class="mui-media-body">图片分享</div></router-link></li>
           <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
             <span class="mui-icon mui-icon-chatbubble"></span>
-            <div class="mui-media-body">Chat</div></a></li>
+            <div class="mui-media-body">商品购买</div></a></li>
           <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
             <span class="mui-icon mui-icon-location"></span>
-            <div class="mui-media-body">location</div></a></li>
+            <div class="mui-media-body">留言反馈</div></a></li>
           <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
             <span class="mui-icon mui-icon-search"></span>
-            <div class="mui-media-body">Search</div></a></li>
+            <div class="mui-media-body">查找</div></a></li>
           <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
             <span class="mui-icon mui-icon-phone"></span>
-            <div class="mui-media-body">Phone</div></a></li>
+            <div class="mui-media-body">手机</div></a></li>
         </ul> 
     </div>
 </template>

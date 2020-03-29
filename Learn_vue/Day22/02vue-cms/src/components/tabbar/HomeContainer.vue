73f46f4b2dc6d60caa2776699d0cc507a01e8e1e
1939108122 <1939108122@qq.com>
2020-03-29 23:58:01 +0800

    改造商品购买路由

--- Learn_vue/Day22/02vue-cms/src/components/tabbar/HomeContainer.vue
+++ Learn_vue/Day22/02vue-cms/src/components/tabbar/HomeContainer.vue
@@ -12,9 +12,9 @@
           <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to= "/home/photolist">
             <span class="mui-icon mui-icon-email"><span class="mui-badge">5</span></span>
             <div class="mui-media-body">图片分享</div></router-link></li>
-          <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+          <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="/home/goodslist">
             <span class="mui-icon mui-icon-chatbubble"></span>
-            <div class="mui-media-body">商品购买</div></a></li>
+            <div class="mui-media-body">商品购买</div></router-link></li>
           <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
             <span class="mui-icon mui-icon-location"></span>
             <div class="mui-media-body">留言反馈</div></a></li>
--- Learn_vue/Day22/02vue-cms/src/router.js
+++ Learn_vue/Day22/02vue-cms/src/router.js
@@ -7,6 +7,7 @@ import NewsList from './components/news/NewsList.vue'
 import NewsInfo from './components/news/NewsInfo.vue'
 import PhotoList from './components/photos/PhotoList.vue'
 import PhotoInfo from './components/photos/PhotoInfo.vue'
+import GoodsList from './components/goods/GoodsList.vue'
 var router = new VueRouter({
     routes: [
         {path: '/', redirect:'/home'},
@@ -18,6 +19,7 @@ var router = new VueRouter({
         {path: '/home/newsinfo/:id', component: NewsInfo },
         {path: '/home/photolist', component: PhotoList },
         {path: '/home/photoinfo/:id', component: PhotoInfo },
+        {path: '/home/goodslist', component: GoodsList}
 
     ],
     linkActiveClass: 'mui-active' //覆盖默认的路由高亮的类  默认的类为link-active-class

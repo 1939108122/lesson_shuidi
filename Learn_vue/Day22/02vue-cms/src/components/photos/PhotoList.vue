ef19cae6db02abd8c013a877015222e89f742241
1939108122 <1939108122@qq.com>
2020-03-30 13:28:11 +0800

    在安卓手机调试网站

--- Learn_vue/Day22/02vue-cms/src/components/photos/PhotoList.vue
+++ Learn_vue/Day22/02vue-cms/src/components/photos/PhotoList.vue
@@ -4,7 +4,7 @@
       <div id="sliderSegmentedControl" class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
         <div class="mui-scroll">
           <a :class="['mui-control-item',item.id == 0? 'mui-active':'']" v-for=" item in cates" :key=" item.id" 
-          @click="getPhotoListByCateId(item.id)">
+          @tap="getPhotoListByCateId(item.id)">
               {{ item.title }}
           </a>
         </div>

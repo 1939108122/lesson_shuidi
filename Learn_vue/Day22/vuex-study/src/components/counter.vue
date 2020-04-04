0e8ef4d965d3e31fd0705096aab9de20e74eb451
1939108122 <1939108122@qq.com>
2020-04-04 12:03:38 +0800

    vuex-study

---
+++ Learn_vue/Day22/vuex-study/src/components/counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="button" value="减少" >
+    <input type="button" value="增加">
+    <br>
+    <input type="text" v-model="$store.state.count">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ Learn_vue/Day22/vuex-study/src/main.js
@@ -0,0 +1,25 @@
+// 入口文件
+import Vue from 'vue'
+// 配置vuex的步骤
+// 1. 运行 cnpm i vuex -S 
+// 2. 导入包
+import Vuex from 'vuex'
+// 3. 注册vuex到vue中
+Vue.use(Vuex)
+
+var store = new Vuex.Store({
+  state: { //state就相当于组件中的data，专门用来存储数据的
+    count:0
+  },
+  mutations: {
+  },
+})
+
+
+import App from './App.vue'
+
+const vm = new Vue({
+  el: '#app',
+  render: c => c(App),
+  store // 5. 将 vuex 创建的 store 挂载到 VM 实例上， 只要挂载到了 vm 上，任何组件都能使用 store 来存取数据
+})

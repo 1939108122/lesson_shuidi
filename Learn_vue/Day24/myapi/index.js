43c643f446dd023d28830df1fceb5a8aec21cea6
1939108122 <1939108122@qq.com>
2020-04-11 15:35:07 +0800

    async&await处理多个异步请求

--- Learn_vue/Day24/myapi/index.js
+++ Learn_vue/Day24/myapi/index.js
@@ -17,7 +17,7 @@ app.all('*', function (req, res, next) {
   next();
 });
 app.get('/async1', (req, res) => {
-  res.send('hello1')
+  res.send('hello')
 })
 app.get('/async2', (req, res) => {
   if(req.query.info == 'hello') {

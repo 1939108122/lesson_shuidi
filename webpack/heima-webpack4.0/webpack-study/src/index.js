254abaee5130dadc502acc2b5d1c86b0ae3ce9c7
1939108122 <1939108122@qq.com>
2020-04-11 21:57:17 +0800

    webpack打包发布

--- webpack/heima-webpack4.0/webpack-study/src/index.js
+++ webpack/heima-webpack4.0/webpack-study/src/index.js
@@ -2,6 +2,8 @@ import $ from 'jquery'
 import './css/1.css'
 import './css/1.less'
 import './css/1.scss'
+
+
 $(function() {
   $('li:odd').css('backgroundColor', 'yellow')
   $('li:even').css('backgroundColor', 'lightblue')
@@ -11,3 +13,12 @@ class Person {
   static name = '小白'
 }
 console.log(Person.name)
+
+
+import Vue from 'vue'
+import App from './components/app.vue'
+
+const vm = new Vue({
+  el:'#app',
+  render: h=>h(App)
+})
--- webpack/heima-webpack4.0/webpack-study/webpack.config.js
+++ webpack/heima-webpack4.0/webpack-study/webpack.config.js
@@ -4,9 +4,10 @@ const htmlplugin =  new HtmlWebpackPlugin({
   template:'./src/index.html',
   filename:'index.html'
 })
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 module.exports = {
   mode: 'development',
-  plugins:[htmlplugin],
+  plugins:[htmlplugin, new VueLoaderPlugin()],
   entry: path.join(__dirname, './src/index.js'),
   output: {
     path: path.join(__dirname, './dist'),
@@ -19,7 +20,8 @@ module.exports = {
         { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader' ] },
         { test:/\.(jpg|png|gif|bmp|jpeg)$/, use: 'url-loader?limit=17800&name=[hash:8]-[name].[ext]'},
         // { test: /\.(ttf|eot|svg|woff|woff2)$/, use: 'url-loader' }, //处理字体文件的loader 
-        { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ } // exclude排除node_modules文件下编译好的js文件，提高执行效率
+        { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ }, // exclude排除node_modules文件下编译好的js文件，提高执行效率
+        { test: /\.vue$/, use: 'vue-loader'}
     ]
 },
 }

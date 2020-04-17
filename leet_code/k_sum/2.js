73114a638d2637ebc844a277e0ac22f7354d8f93
1939108122 <1939108122@qq.com>
2020-04-17 12:53:16 +0800

    理解

---
+++ leet_code/k_sum/2.js
@@ -0,0 +1,20 @@
+
+// var test = function (a) {
+//   if(a.length > 0)
+//   {
+//     if (a[0]>0)
+//     {
+//       a[0] =3
+//     }
+//     return 0;  //return 是退出函数 break是退出本层循环（多层循环表示退出当层循环执行外层循环），continue是退出本次循环
+//   }
+//   for (var i=0; i<a.length-1; i++)
+//     {
+//       console.log(a[i])
+//     }
+// }
+
+// console.log(test([1,2,3,4,5]))
+var a1 = [4,5,6]
+a1 = a1.slice(1,1)
+console.log(a1)

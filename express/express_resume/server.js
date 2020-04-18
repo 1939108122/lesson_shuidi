89da52791a44bd72c98a69ef9ba44d1138182cd2
1939108122 <1939108122@qq.com>
2020-04-18 10:47:40 +0800

    express-demo

--- express/express_resume/server.js
+++ express/express_resume/server.js
@@ -14,7 +14,7 @@ const express = require('express')
 const hostname = 'localhost';
 const port = 3000;
 const app = express()
-app.set('views', 'views')  //指定模板存放目录
+app.set('views', 'views')               //指定模板存放目录
 app.set('view engine', 'hbs');          // 指定模板引擎为handlebars
 
 
@@ -24,6 +24,7 @@ function loggingMiddleware(req, res, next) {
   next();
 }
 app.use(loggingMiddleware)
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -36,6 +37,23 @@ app.get('/contact', (req, res) => {
 app.get('/', (req, res) => {
   res.send("hello, world!")
 })
+app.get('/broken', (req, res) => {
+  throw new Error('Broken!');
+});
+
+app.get('/api', (req, res) => {
+  res.json({ name: '图雀社区', website: 'https://tuture.co' });
+});
+
+app.use('*', (req, res) => {
+  res.status(404).render('404', { url: req.originalUrl });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render('500');
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });

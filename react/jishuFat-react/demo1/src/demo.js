1bfe0eb9fe05039c81b306593bc7d6de392c883c
1939108122 <1939108122@qq.com>
2020-04-21 15:38:31 +0800

    component组件的拆分

--- react/jishuFat-react/demo1/src/demo.js
+++ react/jishuFat-react/demo1/src/demo.js
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from 'react'
+import Demo1 from './demo1'
+import './style.css'
 
 class Xiaojiejie extends Component {
   constructor(props) {
@@ -12,16 +14,18 @@ class Xiaojiejie extends Component {
     return (
      <Fragment>
         <div>
-          <input value={ this.state.inputValue } onChange={ this.inputChange.bind(this) }/>
+          <label htmlFor="focus">增加服务</label>
+          <input  id="focus" value={ this.state.inputValue } onChange={ this.inputChange.bind(this) }/>
           <button onClick={this.addList.bind(this)}>增加服务</button>
         </div>
         <ul>
           {
             this.state.list.map((item, index) =>{
-            return <li 
-                    key={index+item} onClick={this.deleteItem.bind(this, index)}>
-                      { item }
-                    </li>
+            return <li>
+                    <div>
+                      <Demo1/>
+                    </div>
+                   </li>
             })
           }
         </ul>
---
+++ react/jishuFat-react/demo1/src/demo1.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Demo1 extends Component {
+  render() { 
+    return ( 
+      <li>Demo1</li>
+     );
+  }
+}
+ 
+export default Demo1;

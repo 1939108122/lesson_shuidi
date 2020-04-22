6db8acf30e5740661f528ffdfd46030f4a230f4a
1939108122 <1939108122@qq.com>
2020-04-22 22:44:22 +0800

    three.js

--- react/react-three/src/client.js
+++ react/react-three/src/client.js
@@ -1,9 +1,11 @@
-// console.log('dsadasda')
+// console.log('dfsdf');
 // var a = 1
 // document.body.innerHTML = a
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import GameContainer from '../container/getContainer'
+// react 组件分为很多种  容器型组件  redux 
+import GameContainer from './containers/GameContainer'
+
 ReactDOM.render(
   <GameContainer/>,
   document.getElementById('game')
--- react/react-three/src/containers/1GameContainer.js
+++ react/react-three/src/containers/1GameContainer.js
@@ -6,19 +6,32 @@ import React3 from 'react-three-renderer'
 export default class GameContainer extends Component {
   constructor(props, context) {
     super(props, context)
-    this.cameraPositon = new THREE.Vector3(0, 0, 5) //三维空间
+    this.cameraPositon = new THREE.Vector3(0, 0, 1) //三维空间
     this.state = {
+      // 运动  几何体 x, y z 
+      cubeRotation: new THREE.Euler() // mvvm 
+    }
+    this._onAnimate = () => {
+        this.setState({
+          cubeRotation: new THREE.Euler(
+            this.state.cubeRotation.x + 0.1,
+            this.state.cubeRotation.y + 0.1,
+            0
+          )
+        })
     }
   }
   render() {
     const width = window.innerWidth,
       height = window.innerHeight
-
+    console.log(width, height)
     return (
       <React3
         mainCamera="camera"
         width={width}
-        height={height}>
+        height={height}
+        onAnimate={this._onAnimate}
+        >
         <scene>
           <perspectiveCamera 
             name="camera"
@@ -28,13 +41,15 @@ export default class GameContainer extends Component {
             far={1000}
             position={this.cameraPositon}
           />
-          <mesh>
+          <mesh
+            rotation={this.state.cubeRotation}
+          >
             <boxGeometry
             width={1}
             height={1}
             depth={1}>
             </boxGeometry>
-            <meshBasicMaterial color={0x00ff00}/>
+            <meshBasicMaterial color={0xff1200}/>
           </mesh>
         </scene>
       </React3>

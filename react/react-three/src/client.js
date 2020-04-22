cfbb45dd1d8a059a23ee6f23bb3e6ff250e0a6ba
1939108122 <1939108122@qq.com>
2020-04-22 18:05:12 +0800

    three

---
+++ react/react-three/src/client.js
@@ -0,0 +1,10 @@
+// console.log('dsadasda')
+// var a = 1
+// document.body.innerHTML = a
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import GameContainer from '../container/getContainer'
+ReactDOM.render(
+  <GameContainer/>,
+  document.getElementById('game')
+)

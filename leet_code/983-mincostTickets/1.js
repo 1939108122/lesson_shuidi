dd7331d36a71b9ac22dd9e00a48f02b11b627dfa
1939108122 <1939108122@qq.com>
2020-05-07 11:27:11 +0800

    力扣983

--- leet_code/983-mincostTickets/1.js
+++ leet_code/983-mincostTickets/1.js
@@ -1,22 +1,26 @@
 var mincostTickets = function(days, costs) {
-  let dp = new Array(366 + 30).fill(0),
-      n = days.length,
-      maxDay = days[n - 1],
-      minDay = days[0],
-      k = n - 1;
-  
-  for (let i = maxDay; i >= minDay; i--) {
-    if (i === days[k]) {
+  let dp = new Array(366+30).fill(0),
+    len = days.length,
+    maxDays = days[len -1],
+    minDays = days[0],
+    k = len-1;
+    
+  for (var i =maxDays; i>=minDays; i--)
+  {
+    if (i === days[k])
+    {
       dp[i] = Math.min(
-        dp[i + 1] + costs[0],
-        dp[i + 7] + costs[1],
-        dp[i + 30] + costs[2]
-      );
-      k--;
-    } else {
-      dp[i] = dp[i + 1];
+        dp[i+1] + costs[0],
+        dp[i+7] + costs[1],
+        dp[i+30] + costs[2]
+      )
+      k--
+    }
+    else{
+      dp[i] = dp[i+1]
     }
   }
-  
-  return dp[minDay];
+  return dp[minDays]
+
 };
+console.log(mincostTickets([1,4,6,7,8,365], [2,7,15]))

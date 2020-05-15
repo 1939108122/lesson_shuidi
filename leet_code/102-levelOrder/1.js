424992d38246ec79c5f09a13a4ce23563e3edbb6
1939108122 <1939108122@qq.com>
2020-05-15 08:59:41 +0800

    力扣102

--- leet_code/102-levelOrder/1.js
+++ leet_code/102-levelOrder/1.js
@@ -1,18 +1,37 @@
-var levelOrder = function(root) {
-  if(root==null)
-    return []
-  var arr=[root]
-  var res=[]
-  while(arr.length>0){
-    var n=arr.length
-    var now=[]
-    while(n-->0){
-      var node=arr.shift()
-      now.push(node.val)
-      if(node.left!=null)arr.push(node.left)
-      if(node.right!=null)arr.push(node.right)
+function Tree(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+var levelOrder = function (root) {
+  if (!root) return []
+  let quete = [root]
+  let res = []
+  while (quete.length)
+  {
+    let subRes = []
+    let len = quete.length
+    for (let i = 0; i<len; i++)
+    {
+      let cur = quete.shift()
+      subRes.push(cur.val)
+      if (cur.left) quete.push(cur.left)
+      if (cur.right) quete.push(cur.right)
     }
-    res.push(now)
+    res.push(subRes)
   }
   return res
-};
+}
+
+const t1 = new Tree(1)
+const t2 = new Tree(2)
+const t3 = new Tree(3)
+const t4 = new Tree(4)
+const t5 = new Tree(5)
+
+t1.left = t2
+t1.right = t3
+t3.left = t4
+t3.right = t5
+
+console.log(levelOrder(t1))

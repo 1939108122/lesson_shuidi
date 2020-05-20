20337fe136d0d57bc2926a9e4d9b82243d70918d
1939108122 <1939108122@qq.com>
2020-05-20 10:38:22 +0800

    力扣152

--- leet_code/152-maxProduct/1.js
+++ leet_code/152-maxProduct/1.js
@@ -1,13 +1,16 @@
-var maxProduct = function(nums) {
-  let max = nums[0];
-  let min = nums[0];
-  let res = nums[0];
-
-  for (let i = 1; i < nums.length; i++) {
-    let tmp = min;
-    min = Math.min(nums[i], Math.min(max * nums[i], min * nums[i])); // 取最小
-    max = Math.max(nums[i], Math.max(max * nums[i], tmp * nums[i])); /// 取最大
-    res = Math.max(res, max);
+function maxProduct(nums)
+{
+  let res = nums[0]
+  let preMin = nums[0]
+  let preMax = nums[0]
+  for (let i=1; i<nums.length; i++)
+  {
+    t1 = preMin*nums[i]
+    t2 = preMax*nums[i]
+    preMin = Math.min(t1, t2, nums[i])
+    preMax = Math.max(t1, t2, nums[i])
+    res = Math.max(preMax, res)
   }
-  return res;
-};
+  return res
+}
+console.log(maxProduct([2,3,-5,-8]))

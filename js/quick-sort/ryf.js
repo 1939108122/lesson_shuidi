04b8eee4012e39763c756590e85ecc2bc8c39af8
1939108122 <1939108122@qq.com>
2020-05-26 23:11:41 +0800

    快速排序

---
+++ js/quick-sort/ryf.js
@@ -0,0 +1,19 @@
+const arr = [0, 1, 9, 8, 4];
+
+// 一次快排 能够给一个 
+// let provit = 
+// k 数组的索引
+// 1. provit 找到正确的位置(数组里面的索引)  !!!小算法 partition 分区
+// 2. 左边 小于 provit 的数 < provit <  右边 大于 provit 的数 
+// 原地交换数组里面两个数据
+const quickSort = function(arr) {
+  if (arr.length <= 1) return arr;
+  let provit = arr[0];
+  let left = [], right = [];
+  for (let i = 1; i < arr.length; i ++) {
+    if (arr[i] < provit) left.push(arr[i]);
+    if (arr[i] > provit) right.push(arr[i]);
+  }
+  return [...(quickSort(left)) , provit, ...(quickSort(right))]
+}
+console.log(quickSort(arr));

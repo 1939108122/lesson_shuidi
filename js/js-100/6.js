01cd87b967f54bb74f4958811e1aa12720d8fe43
1939108122 <1939108122@qq.com>
2020-05-31 22:58:59 +0800

    数据劫持

---
+++ js/js-100/6.js
@@ -0,0 +1,24 @@
+//  MVVM 数据劫持
+// var arr = [1];
+// arr[10000] = 1;
+
+
+// function a() {
+//   console.time();
+//   for (var i = 0; i < arr.length; i++) {
+//     console.log(1)
+//   }
+//   console.timeEnd();
+// }
+
+
+// a();
+
+// function b() {
+//   console.time();
+//   arr.forEach(item => { console.log(2) })
+//   console.timeEnd();
+// }
+// b();
+
+

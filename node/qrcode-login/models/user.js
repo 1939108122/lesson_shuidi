d7095ef3b29444817733d5218a258c9bb40fbe59
1939108122 <1939108122@qq.com>
2020-06-03 23:12:47 +0800

    qrcoe-login

---
+++ node/qrcode-login/models/user.js
@@ -0,0 +1,11 @@
+const Schema = mongoose.Schema;
+
+
+const UserSchema = new Schema({
+  username: String,
+  password: String,
+  token: String
+})
+
+
+module.exports = mongoose.model('User', UserSchema);
